9519ebef0a38120240e6dcf082a22161ad51fe90
Mathanesh210 <86698920+mathanesh210@users.noreply.github.com>
2022-05-13 16:10:22 +0530

    Add files via upload

---
+++ shakthivel.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int regno it_mark,n,i=1;
+    char name[20];
+    printf("student grade validation\n");
+    student_details;
+    {
+        printf("\n enter name of the student:");
+        scanf("%s",&name);
+        printf("\n enter student register number:")
+        scanf("%d",&regno);
+        printf("\n enter %s total mark obtained:",name);
+        scanf("%d",&t_mark);
+        if (200<=t_mark)
+        {
+            printf("\n1)450to500/t 2)400to450/t 3)350to400/t 4)300to350/t 5)200to300");
+            printf("\n choose your mark range:");
+            scanf("%d",&n);
+            switch(n)
+            {
+                case1:
+                printf("\n your rank is FIRST clan and your are passing with A grade");
+                break;
+                case2:
+                printf("\n your rank is FIRST clan and are passing with B grade");
+                break;
+                case3:
+                printf("\n your rank is FIRST clan and you are passing with C grade");
+                break;
+                case4;
+                printf("\n your rank is SECOND clan and you are passing D grade");
+                break;
+                case5:
+                printf("\n your rank is SECOND clan and you are passing");
+                break;
+
+
+            }
+            printf("\n CONSRATULATIONS!!!");
+            IF(499==t_mark)
+            {
+                printf("\n best wishes !!!\n you are a state first to your state !!!");
+            }
+             else
+              {
+                  printf("\n best wishes !!!\n");
+              }
+             }
+              else
+              {
+                  printf("\n sorry you are tail,beter luck next time !!!\n");
+              }
+             }
+              i++;
+              if(i==1)
+              {
+                  goto students_details;
+              }
+                printf("\n continue satements \n");
+                for(i=1;i<=10;i++)
+                {
+                    if(i==5)
+                    {
+                        continue;
+                        }
+                         printf("%d\n',i");
+                         }
+                          printf("end program");
+                          }
+                    
+                
+             
+
+
